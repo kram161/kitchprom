2b031f082ff98d47728daadaf8e74d44af2e94b9
kram161 <39655243+kram161@users.noreply.github.com>
2021-12-07 14:37:42 +0300

    upd

---
+++ tasks/scss2css.js
@@ -0,0 +1,34 @@
+const { src, dest } = require('gulp');
+
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+
+module.exports = function scss2css() {
+	var plugins = [
+		autoprefixer({
+			overrideBrowserslist: ['last 8 versions'],
+			browsers: [
+				'Android >= 4',
+				'Chrome >= 20',
+				'Firefox >= 24',
+				'Explorer >= 11',
+				'iOS >= 6',
+				'Opera >= 12',
+				'Safari >= 6',
+			],
+		}),
+	];
+
+	return src('app/assets/styles/**/*.scss')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(concat('styles.min.css'))
+		.pipe(postcss(plugins))
+		.pipe(sourcemaps.write('/sourcemaps/'))
+		.pipe(dest('build/assets/css'));
+};
